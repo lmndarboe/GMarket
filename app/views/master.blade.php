976eb5cd6891eb283327fd762a5bbc9ef619fdc4
Lamin Darboe <lmndarboe@gmail.com>
2013-12-30 21:06:09 +0000

    Changing the Site logo

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -14,7 +14,7 @@
 
 <div id="header">
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-	<a class="navbar-brand" href="#">G-Market</a>
+	<a class="navbar-brand three-d" href="#">G-Market</a>
 	<p class="navbar-text navbar-right"><a href="#" class="btn btn-success">Log In</a>&nbsp;  <a href="#" class="btn btn-warning">Sign Up</a></p>
 
   <ul class="nav navbar-nav">
